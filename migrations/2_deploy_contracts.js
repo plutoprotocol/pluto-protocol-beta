763b000ca3fdf44ea2a976185009cb11d9193f9c
Ashton <oncwnuBhYMCivjUMIRRtEeLBwvlc@git.weixin.qq.com>
2020-09-12 19:26:43 +0800

    add script to deploy contracts to ropsten

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -25,4 +25,23 @@ module.exports = async function(deployer, network, accounts) {
         let allSupportedMarkets = await riskManagerInstance.getAllMarkets();
         console.log(allSupportedMarkets);
     }
+
+    if (network == "ropsten") {
+        let tetherToken = "0xf17D721369F540f7485E1033194eBf92e3f88079";
+        await deployer.deploy(MockNest3VoteFactory);
+        await deployer.deploy(NestPriceOracle, MockNest3VoteFactory.address);
+        await deployer.deploy(RiskManager).then(async function (instance) {
+            await instance._setPriceOracle(NestPriceOracle.address);
+            oracle = await instance.oracle();
+        });
+        await deployer.deploy(InterestModel, "20000000000000000", "200000000000000000");
+        await deployer.deploy(pETH, RiskManager.address, InterestModel.address, "1000000000000000000", "Pluto ETH", "pETH", 18, accounts[0])
+        await deployer.deploy(pUSDT, tetherToken, RiskManager.address, InterestModel.address, "1000000", "Pluto USDT", "pUSDT", 18, accounts[0])
+
+        let riskManagerInstance = await RiskManager.deployed();
+        await riskManagerInstance._supportMarket(pETH.address);
+        await riskManagerInstance._supportMarket(pUSDT.address);
+        let allSupportedMarkets = await riskManagerInstance.getAllMarkets();
+        console.log(allSupportedMarkets);
+    }
 };
