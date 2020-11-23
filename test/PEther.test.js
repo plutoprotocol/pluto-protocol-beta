9b57706e89f9897f77218e1d329e226de01847d2
Ashton <oncwnuBhYMCivjUMIRRtEeLBwvlc@git.weixin.qq.com>
2020-11-23 10:20:11 +0800

    add more tests

--- test/PEther.test.js
+++ test/PEther.test.js
@@ -1,6 +1,7 @@
 const PEther = artifacts.require("PEther");
 const PUsdt = artifacts.require("PErc20");
 const RiskManager = artifacts.require("RiskManager");
+const Usdt = artifacts.require("TetherToken");
 
 contract("PEther", async accounts => {
     it("should redeemUnderlying properly", async () => {
@@ -29,5 +30,25 @@ contract("PEther", async accounts => {
         assert.equal(cash, 0);
         balanceOfUnderlying = await pEtherInstance.balanceOfUnderlying.call(account);
         assert.equal(balanceOfUnderlying, 0);
+    });
+
+    it("should repay properly", async () => {
+        let pEtherInstance = await PEther.deployed();
+        let pUsdtInstance = await PUsdt.deployed();
+        let riskMgrInstance = await RiskManager.deployed();
+        let usdtInstance = await Usdt.deployed();
+        let account = accounts[0];
+
+        await riskMgrInstance.enterMarkets([pEtherInstance.address, pUsdtInstance.address]);
+        let priceCost = await riskMgrInstance.getPriceCost(account);
+        let depositAmount = web3.utils.toWei('0.1', 'ether');
+        await pEtherInstance.mint({value: depositAmount})
+        await usdtInstance.approve(pUsdtInstance.address, 100e6.toString());
+        await pUsdtInstance.mint(100e6.toString());
+        await pUsdtInstance.borrow(10e6.toString(), {value: priceCost});
+        let borrowBalance = await pUsdtInstance.borrowBalanceCurrent.call(account);
+        console.log(`borrowBalance: ${borrowBalance}`);
+        await usdtInstance.approve(pUsdtInstance.address, borrowBalance);
+        await pUsdtInstance.repayBorrow(borrowBalance);
     });
 })
