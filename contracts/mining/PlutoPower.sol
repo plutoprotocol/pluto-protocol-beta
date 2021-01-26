dabeabf5c6f88409cf68cf4ae0f3e17a76845623
Ashton <oncwnuBhYMCivjUMIRRtEeLBwvlc@git.weixin.qq.com>
2021-01-27 00:09:00 +0800

    remove mining folder

--- contracts/mining/PlutoPower.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.6.9;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract PlutoPower is ERC20 {
-    constructor(address initialOwner, uint256 initialSupply) public ERC20("Pluto Power", "PP") {
-        _mint(initialOwner, initialSupply);
-    }
-}
--- contracts/mining/PlutoToken.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.6.9;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract PlutoToken is ERC20 {
-    constructor(uint256 initialSupply) public ERC20("Pluto Finance Token", "PLUF") {
-        _mint(msg.sender, initialSupply);
-    }
-}
